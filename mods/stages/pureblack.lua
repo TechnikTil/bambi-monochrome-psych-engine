bad2338ce18132e9e72a9d1fcbbc3cf80e184b21
TechnikTil <89487150+TechnikTil@users.noreply.github.com>
2022-01-08 14:09:07 -0700

    Updating the mod to 2.5

--- mods/stages/pureblack.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	makeLuaSprite('juststuff', 'bambi', -160, -200);
-	setScrollFactor('juststuff', 0.9, 0.9);
-	scaleObject('juststuff', 2.5, 2.5);
-	--The background for bambi
-
-    addLuaSprite('juststuff', false);
-		
-end
